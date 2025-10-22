42bd6aa4658fd379a7076122b67a2d782b2801f2
Gemma215 <iheomachisom.ugwunweze@pau.edu.ng>
2025-10-22 13:28:15 +0100

    (Practice_1)

---
+++ Week-4/Practice_1/target/debug/Practice_1.d
@@ -0,0 +1,1 @@
+C:\Rust\ Projects\u.iheomachisomCOS101\Week-4\Practice_1\target\debug\Practice_1.exe: C:\Rust\ Projects\u.iheomachisomCOS101\Week-4\Practice_1\src\main.rs
---
+++ Week-4/Practice_1/target/debug/deps/Practice_1.d
@@ -0,0 +1,5 @@
+C:\Rust Projects\u.iheomachisomCOS101\Week-4\Practice_1\target\debug\deps\Practice_1.d: src\main.rs
+
+C:\Rust Projects\u.iheomachisomCOS101\Week-4\Practice_1\target\debug\deps\Practice_1.exe: src\main.rs
+
+src\main.rs:
